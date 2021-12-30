70d83d3acca00f69feb4b1d32558331efd524baf
Dada Khalandar <kerry.blig12@gmail.com>
2021-12-30 15:41:43 +0530

    homepage done

--- src/components/Connect/Connect.tsx
+++ src/components/Connect/Connect.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Image, Grid, Box } from '@chakra-ui/react'
+import { Image, Box } from '@chakra-ui/react'
 import section from '../../assets/images/working-section.svg'
+import shop from '../../assets/images/shop.svg'
 
 
 const Connect = () => {
     return (
         <>
-            <Box mt='35%' mb='-32%'>
+            <Box mt='35%' mb='-75%'>
                 <Image src={section} alt='section' w='100%' />
+                <Image pos='relative' bottom='60rem' left='50%' w='55rem' h='50rem' src={shop} alt='section' />
 
             </Box>
         </>
