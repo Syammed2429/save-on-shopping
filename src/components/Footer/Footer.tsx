0d56e62009d1f0be956abffd4bd668983661f96f
Dada Khalandar <kerry.blig12@gmail.com>
2021-12-30 14:38:44 +0530

    footer is done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
-import { Container, Image } from '@chakra-ui/react'
+import { Box, Container, Flex, Image, SimpleGrid, Stack, Text, VStack } from '@chakra-ui/react'
 import { FC } from 'react'
 import logo from '../../assets/images/logo2.svg'
+import { FaFacebookF } from 'react-icons/fa'
+import { BsInstagram } from 'react-icons/bs'
 
 
 
@@ -8,10 +10,33 @@ import logo from '../../assets/images/logo2.svg'
 const Footer: FC = () => {
     return (
         <>
-            <Container bg='#C4C4C4'>
-                <Image src={logo} />
+            {/* <VStack bg='#C4C4C4' > */}
+            <Container bg='#C4C4C4' h='250px' pos='absolute' left='0' right='0' mt='25px' >
+
+                <SimpleGrid columns={3} spacingX='40px' spacingY='20px'>
+                    <Box height='80px'>
+                        <Image src={logo} />
+
+                    </Box>
+                    <Box height='80px'>
+                        <Stack >
+                            <Text>About us</Text>
+                            <Text>Contact us</Text>
+                            <Text>Advertisement</Text>
+                        </Stack >
+                    </Box>
+                    <Box height='80px'>
+                        <Text>Follow us on</Text>
+                        <Flex>
+                            <FaFacebookF />
+                            <BsInstagram />
+                        </Flex>
+                    </Box>
+
+                </SimpleGrid>
 
             </Container>
+            {/* </VStack> */}
 
         </>
     )
